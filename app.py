e018d423d8e5eb40ce1948acea730dff1a1d54ca
phong vu <54299527+phongvu009@users.noreply.github.com>
2023-01-13 20:07:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,20 @@ class Image(Resource):
 #endpoint
 api.add_resource(Image,'/predict')
 
+class Quotes(Resource):
+    def get(self):
+        return {
+            'William Shakespeare': {
+                'quote': ['Love all,trust a few,do wrong to none',
+		'Some are born great, some achieve greatness, and some greatness thrust upon them.']
+        },
+        'Linus': {
+            'quote': ['Talk is cheap. Show me the code.']
+            }
+        }
+
+api.add_resource(Quotes, '/')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
